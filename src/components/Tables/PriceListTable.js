635cec5fefd8ae984ff4840545bacec00c71c611
JIrayu Promsongwong <32473206+jirayuboss@users.noreply.github.com>
2022-12-27 07:14:17 +0700

    Add edit table row feature / change file AddTransportPriceDialog to .jsx / add key to component in navbar and PriceListTable (#14)
    * Add edit table row feature / change file AddTransportPriceDialog to .jsx / add key to component in navbar and PriceListTable
    
    * Remove console.log that cause a bug
    
    * Add condition to make it not conflict with add new row function

--- src/components/Tables/PriceListTable.js
+++ src/components/Tables/PriceListTable.js
@@ -13,67 +13,69 @@ export default function PriceListTable(props) {
           borderCollapse: "collapse",
         }}
       >
-        <tr style={{ border: "1px solid black", borderCollapse: "collapse" }}>
-          <th
-            style={{ border: "1px solid black", borderCollapse: "collapse" }}
-          />
-          {props.priceListArr[0].value.map((value, index) => {
-            return (
-              <th
-                key={`col-${index}`}
-                style={{
-                  border: "1px solid black",
-                  borderCollapse: "collapse",
-                  height: "30px",
-                }}
-              >
-                {(0.25 * (index + 1)).toFixed(2)}
-              </th>
-            );
-          })}
-        </tr>
+        <tbody>
+          <tr style={{ border: "1px solid black", borderCollapse: "collapse" }}>
+            <th
+              style={{ border: "1px solid black", borderCollapse: "collapse" }}
+            />
+            {props.priceListArr[0].value.map((value, index) => {
+              return (
+                <th
+                  key={`col-${index}`}
+                  style={{
+                    border: "1px solid black",
+                    borderCollapse: "collapse",
+                    height: "30px",
+                  }}
+                >
+                  {(0.25 * (index + 1)).toFixed(2)}
+                </th>
+              );
+            })}
+          </tr>
 
-        <React.Fragment>
-          {props.priceListArr.map((yValue, index) => (
-            <tr
-              style={{
-                border: "1px solid black",
-                borderCollapse: "collapse",
-              }}
-              key={`${yValue.name}-${index}`}
-            >
-              <td
+          <React.Fragment>
+            {props.priceListArr.map((yValue, index) => (
+              <tr
                 style={{
                   border: "1px solid black",
                   borderCollapse: "collapse",
-                  textAlign: "center",
-                  height: "30px",
-                  minWidth: "40px",
                 }}
+                key={`${yValue.name}-${index}`}
               >
-                {yValue.name}
-              </td>
-              {props.priceListArr
-                .filter((obj) => obj.name === yValue.name)[0]
-                .value.map((data, index) => {
-                  return (
-                    <td
-                      key={`${data}-${index}`}
-                      style={{
-                        border: "1px solid black",
-                        borderCollapse: "collapse",
-                        textAlign: "center",
-                        height: "30px",
-                        minWidth: "70px",
-                      }}
-                    >
-                      {data.toFixed(2)}
-                    </td>
-                  );
-                })}
-            </tr>
-          ))}
-        </React.Fragment>
+                <td
+                  style={{
+                    border: "1px solid black",
+                    borderCollapse: "collapse",
+                    textAlign: "center",
+                    height: "30px",
+                    minWidth: "40px",
+                  }}
+                >
+                  {yValue.name}
+                </td>
+                {props.priceListArr
+                  .filter((obj) => obj.name === yValue.name)[0]
+                  .value.map((data, index) => {
+                    return (
+                      <td
+                        key={`${data}-${index}`}
+                        style={{
+                          border: "1px solid black",
+                          borderCollapse: "collapse",
+                          textAlign: "center",
+                          height: "30px",
+                          minWidth: "70px",
+                        }}
+                      >
+                        {data.toFixed(2)}
+                      </td>
+                    );
+                  })}
+              </tr>
+            ))}
+          </React.Fragment>
+        </tbody>
       </table>
     </div>
   );
