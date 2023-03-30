ff62e9dadb581e2169b79277275120222dbd56bf
Arniwatt Chonkiattipoom <pruekanw@gmail.com>
2023-03-30 17:55:39 +0700

    chore: align same date-picker in transportPrice with other pages

--- src/components/DynamicDialogContent/index.jsx
+++ src/components/DynamicDialogContent/index.jsx
@@ -7,9 +7,93 @@ import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
+//Date picker
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+import * as CalendarConstants from "../../constants/CalendarConstants";
+
+function SingleSelectInput(props) {
+    const { column, inputObj, onChangeInput } = props;
+  
+    return (
+      <Select
+        id={column.field}
+        name={column.field}
+        value={inputObj[column.field] || ''}
+        onChange={onChangeInput}
+        sx={{ width: "100%" }}
+        size="small"
+      >
+        {column.valueOptions.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </Select>
+    );
+}
+
+function DateInput(props) {
+    const { column, inputObj, onChangeInput } = props;
+  
+    const handleChange = (date) => {
+      onChangeInput({ target: { name: column.field, value: date } });
+    };
+  
+    return (
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <MobileDatePicker
+          label={column.label || ''}
+          inputFormat={CalendarConstants.dateFormat}
+          value={inputObj[column.field] || null}
+          onChange={handleChange}
+          renderInput={(params) => (
+            <TextField
+              size="small"
+              sx={{ width: '100%' }}
+              variant="outlined"
+              error={props.isError && !inputObj[column.field]}
+              name={column.field}
+              {...params}
+            />
+          )}
+        />
+      </LocalizationProvider>
+    );
+}
+
+function DefaultInput(props) {
+    const { column, inputObj, onChangeInput } = props;
+  
+    return (
+      <TextField
+        sx={{ width: "100%" }}
+        id={column.field}
+        name={column.field}
+        type={column.type || 'input'}
+        variant="outlined"
+        error={props.isError && !inputObj[column.field]}
+        onChange={onChangeInput}
+        value={inputObj[column.field] || ''}
+        size="small"
+      />
+    );
+}
+
+const getInputComponent = (inputObj, onChangeInput, column) => {
+    switch (column.type) {
+        case "singleSelect":
+          return <SingleSelectInput inputObj={inputObj} onChangeInput={onChangeInput} column={column} />;
+        case "date":
+          return <DateInput inputObj={inputObj} onChangeInput={onChangeInput} column={column} />;
+        default:
+          return <DefaultInput inputObj={inputObj} onChangeInput={onChangeInput} column={column} />;
+      }
+}
+
 export function DynamicDialogContent(props) {
     const filteredColumns = props.columns.filter(column => !props.excludeFields.includes(column.field));
-
     return (
         <Grid container spacing={2}>
             {filteredColumns.map((column) => (
@@ -17,32 +101,7 @@ export function DynamicDialogContent(props) {
                     <Typography variant="subtitle1" gutterBottom>
                         {column.headerName}
                     </Typography>
-                    {column.type === 'singleSelect' ? (
-                        <Select
-                            id={column.field}
-                            name={column.field}
-                            value={props.inputObj[column.field] || ''}
-                            onChange={props.onChangeInput}
-                            sx={{ width: "100%" }}
-                        >
-                            {column.valueOptions.map((option) => (
-                                <MenuItem key={option} value={option}>
-                                    {option}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    ) : (
-                        <TextField
-                            sx={{ width: "100%" }}
-                            id={column.field}
-                            name={column.field}
-                            type={column.type || 'input'}
-                            variant="outlined"
-                            error={props.isError && !props.inputObj[column.field]}
-                            onChange={props.onChangeInput}
-                            value={props.inputObj[column.field] || ''}
-                        />
-                    )}
+                    {getInputComponent(props.inputObj, props.onChangeInput, column)}
                 </Grid>
             ))}
         </Grid>
