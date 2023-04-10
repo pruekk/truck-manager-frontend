aabc56ae96936309aab0f9968c3f810a05c75de2
jirayuboss <jirayu.promsongwong@hotmail.com>
2023-04-10 22:00:45 +0200

    Calculate sum if it's edit then display sum value

--- src/scenes/TransportPrice/components/AddNewDialog/components/Table/functions/Functions.js
+++ src/scenes/TransportPrice/components/AddNewDialog/components/Table/functions/Functions.js
@@ -1,3 +1,21 @@
 export function calculateSum(array) {
     return array.reduce((accumulator, value) => accumulator + value, 0);
 }
+
+export function calculateSumY(priceListArr) {
+    let tempYArr = [];
+    priceListArr[0].value.map((obj, index) => {
+        let tempArr = [];
+        priceListArr.map((obj, xIndex) => {
+            tempArr[xIndex] = obj.value[index];
+
+            return tempArr;
+        })
+
+        tempYArr[index] = calculateSum(tempArr)
+
+        return tempYArr;
+    });
+
+    return tempYArr;
+}
