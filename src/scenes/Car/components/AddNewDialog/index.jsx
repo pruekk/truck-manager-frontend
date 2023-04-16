2cf345380d097187baada903130ee6dae5b6bb8a
jirayuboss <jirayu.promsongwong@hotmail.com>
2023-04-17 01:28:56 +0700

    Created reuseable AddDialog / Applied to Car and Driver page

--- src/scenes/Car/components/AddNewDialog/index.jsx
+++ src/scenes/Car/components/AddNewDialog/index.jsx
@@ -1,15 +1,7 @@
 import React from "react";
 
-import CloseIcon from "@mui/icons-material/Close";
-import LoadingButton from '@mui/lab/LoadingButton';
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
-import IconButton from "@mui/material/IconButton";
-
 //Components
-import { DynamicDialogContent } from "../../../../components/DynamicDialogContent";
+import AddDialog from "../../../../components/AddDialog";
 
 //Constants
 import * as NavigationBarConstants from "../../../../constants/NavigationBarConstants";
@@ -21,7 +13,7 @@ export default function AddCarDialog(props) {
 
     const [isError, setIsError] = React.useState(false);
     const onChangeInput = (event) => {
-        if ( typeof event !== 'undefined' ) {
+        if (typeof event !== 'undefined') {
             const { name, value } = event.target;
             setCarObj((prevObj) => ({
                 ...prevObj,
@@ -39,8 +31,8 @@ export default function AddCarDialog(props) {
         } else {
             setIsError(false);
             const formattedData = {
-                    ...carObj,
-                    price: Number(carObj["price"])
+                ...carObj,
+                price: Number(carObj["price"])
             }
             await props.handleAddNewCar(formattedData);
         }
@@ -49,44 +41,17 @@ export default function AddCarDialog(props) {
     }
 
     return (
-        <Dialog
-            fullWidth={true}
-            maxWidth="md"
-            open={props.openDialog}
-            onClose={props.handleCloseDialog}
-        >
-            <DialogTitle sx={{ backgroundColor: "#FBFBFB" }}>
-                เพิ่ม{NavigationBarConstants.menus[6].sub[0].name}
-                <IconButton
-                    aria-label="close"
-                    onClick={props.handleCloseDialog}
-                    sx={{
-                        position: "absolute",
-                        right: 8,
-                        top: 8,
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            </DialogTitle>
-            <DialogContent dividers sx={{ backgroundColor: "#FBFBFB" }}>
-                <DynamicDialogContent
-                columns={columns}
-                inputObj={carObj}
-                excludeFields={excludeFields}
-                onChangeInput={onChangeInput}
-                isError={isError} 
-                />
-            </DialogContent>
-            <DialogActions sx={{ backgroundColor: "#FBFBFB" }}>
-                <LoadingButton
-                loading={isLoading}
-                onClick={onClickAdd}
-                >
-                Add
-                </LoadingButton>
-            </DialogActions>
-        </Dialog>
+        <AddDialog
+            pageName={NavigationBarConstants.menus[6].sub[0].name}
+            openDialog={props.openDialog}
+            handleCloseDialog={props.handleCloseDialog}
+            columns={columns}
+            dataObj={carObj}
+            excludeFields={excludeFields}
+            onChangeInput={onChangeInput}
+            isError={isError}
+            isLoading={isLoading}
+            onClickAdd={onClickAdd}
+        />
     );
 }
