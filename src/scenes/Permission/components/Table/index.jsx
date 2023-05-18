47ff22700e8131e70875cf2efac9596fcc3d3fc8
jirayuboss <jirayu.promsongwong@hotmail.com>
2023-05-18 23:35:26 +0700

    Edit design of permission table

--- src/scenes/Permission/components/Table/index.jsx
+++ src/scenes/Permission/components/Table/index.jsx
@@ -8,10 +8,14 @@ import * as TableConstants from '../../../../constants/TableConstants';
 
 export const columns = [
     { field: 'email', headerName: 'อีเมล', minWidth: TableConstants.columnsSize.large },
-    { field: 'get', headerName: 'อ่าน', minWidth: TableConstants.columnsSize.large, valueGetter: (params) => params.row?.actions?.get  },
-    { field: 'post', headerName: 'สร้าง', minWidth: TableConstants.columnsSize.large, valueGetter: (params) => params.row?.actions?.post },
-    { field: 'patch', headerName: 'แก้ไข', minWidth: TableConstants.columnsSize.large, valueGetter: (params) => params.row?.actions?.patch },
-    { field: 'delete', headerName: 'ลบ', minWidth: TableConstants.columnsSize.large, valueGetter: (params) => params.row?.actions?.delete }
+    { field: 'pget', headerName: 'อ่าน', minWidth: TableConstants.columnsSize.small, headerAlign: 'center', align:'center', valueGetter: (params) => params.row?.actions?.get.includes('page') ? '✓' : 'x' },
+    { field: 'ppost', headerName: 'สร้าง', minWidth: TableConstants.columnsSize.small, headerAlign: 'center', align:'center', valueGetter: (params) => params.row?.actions?.post.includes('page') ? '✓' : 'x' },
+    { field: 'ppatch', headerName: 'แก้ไข', minWidth: TableConstants.columnsSize.small, headerAlign: 'center', align:'center', valueGetter: (params) => params.row?.actions?.patch.includes('page') ? '✓' : 'x' },
+    { field: 'pdelete', headerName: 'ลบ', minWidth: TableConstants.columnsSize.small, headerAlign: 'center', align:'center', valueGetter: (params) => params.row?.actions?.delete.includes('page') ? '✓' : 'x' },
+    { field: 'cget', headerName: 'อ่าน', minWidth: TableConstants.columnsSize.small, headerAlign: 'center', align:'center', valueGetter: (params) => params.row?.actions?.get.includes('component') ? '✓' : 'x' },
+    { field: 'cpost', headerName: 'สร้าง', minWidth: TableConstants.columnsSize.small, headerAlign: 'center', align:'center', valueGetter: (params) => params.row?.actions?.post.includes('component') ? '✓' : 'x' },
+    { field: 'cpatch', headerName: 'แก้ไข', minWidth: TableConstants.columnsSize.small, headerAlign: 'center', align:'center', valueGetter: (params) => params.row?.actions?.patch.includes('component') ? '✓' : 'x' },
+    { field: 'cdelete', headerName: 'ลบ', minWidth: TableConstants.columnsSize.small, headerAlign: 'center', align:'center', valueGetter: (params) => params.row?.actions?.delete.includes('component') ? '✓' : 'x' }
 ];
 
 export default function DataTable(props) {
@@ -30,7 +34,15 @@ export default function DataTable(props) {
             columnGroupingModel={[
                 {
                     groupId: 'สิทธิในการเข้าถึง',
-                    children: [{ field: 'get' }, { field: 'post' }, { field: 'patch' }, { field: 'delete' }],
+                    children: [{
+                        groupId: 'หน้าเพจ',
+                        children: [{ field: 'pget' }, { field: 'ppost' }, { field: 'ppatch' }, { field: 'pdelete' }],
+                        headerAlign: 'center',
+                    },{
+                        groupId: 'ส่วนประกอบในเพจ',
+                        children: [{ field: 'cget' }, { field: 'cpost' }, { field: 'cpatch' }, { field: 'cdelete' }],
+                        headerAlign: 'center',
+                    }],
                     headerAlign: 'center',
                 }
             ]}
