0e8d2de62890eb48616b3618f725bfbc0be81ae2
Arniwatt Chonkiattipoom <arniwatt.c@linecorp.com>
2023-08-20 11:31:23 +0700

    chore: Add login page design

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,26 @@
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useNavigate, Link } from "react-router-dom";
+import CompanyLogo from "/company_logo.png";
+import GoogleLogo from "/google.png";
+import "./login.scss"
 
-export default Login
+const Login = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">
+          <img src={CompanyLogo} />
+          <p>Truck Manager</p>
+        </span>
+        <span className="title">Login</span>
+        <button className="loginWithGoogle" onClick={() => navigate("/")}>
+          <img src={GoogleLogo} /> 
+          Sign in with Google
+        </button>
+        <p>You don't have an account? <Link className="link" to="/register">Register</Link></p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
